8dd397572d5757e5c794c1e6d59b0ce3a682e641
Michelle-Gn <michelle.guan93@gmail.com>
2022-01-21 19:04:16 -0800

    Comment out explor function

--- client/dist/components/Explore.jsx
+++ client/dist/components/Explore.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 var explore = function () {
-
+  // make a list of automatically updating activities
 }
 
 export default explore;
